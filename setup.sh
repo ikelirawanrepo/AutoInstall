d285b586c02e6b6b91a867e06107abeb078e1497
ikelirawanrepo <80145880+ikelirawanrepo@users.noreply.github.com>
2021-08-28 22:26:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://github.com/ikelirawanrepo/AutoInstall/raw/main/ipvps.conf | tac | tac | grep $MYIP )
+IZIN=$( curl https://github.com/ikelirawanrepo/AutoInstall/raw/main/ipvps.conf | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
